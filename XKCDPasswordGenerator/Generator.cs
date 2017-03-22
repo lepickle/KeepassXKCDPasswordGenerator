807e273bb1f15df304f5dc8e5ac86999f0546ba3
Cortejo <mark-jethro.cortejo@hpe.com>
2017-03-22 22:22:57 +0800

    Add project files.

---
+++ XKCDPasswordGenerator/Generator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KeePassLib;
+using KeePassLib.Cryptography;
+using KeePassLib.Cryptography.PasswordGenerator;
+using KeePassLib.Security;
+
+namespace XKCDPasswordGenerator
+{
+    public class Generator : CustomPwGenerator
+    {
+        private static readonly PwUuid UUID =
+            new PwUuid(new byte[] {
+                        0x53, 0x81, 0x36, 0x0E, 0xA7, 0xFC, 0x48, 0x36,
+                        0x9E, 0x9F, 0xA4, 0x4F, 0x1A, 0xF0, 0x58, 0x37
+            });
+        public override PwUuid Uuid
+        {
+            get
+            {
+                return UUID;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "XKCDPasswordGenerator";
+            }
+        }
+
+        public override ProtectedString Generate(PwProfile prf, CryptoRandomStream crsRandomSource)
+        {
+            string targetSequence = string.Empty;
+            for (int i = 0; i < 10; i++)
+            {
+                targetSequence += "Hello";
+            }
+            //foreach (SequenceItem sequenceItem in globalConfiguration.Sequence)
+            //{
+            //targetSequence += new ItemGenerator.ItemGenerator(globalConfiguration)
+            //                .Generate(sequenceItem, cryptoRandom);
+            //}
+            return new ProtectedString(true, targetSequence);
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ XKCDPasswordGenerator/XKCDPasswordGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+using KeePass.Plugins;
+
+namespace XKCDPasswordGenerator
+{
+    public sealed class XKCDPasswordGeneratorExt : Plugin
+    {
+        private IPluginHost m_host = null;
+        private ToolStripSeparator m_tsSeparator = null;
+        private ToolStripMenuItem m_tsmiMenuItem = null;
+        private Generator m_gen = null;
+
+        public override bool Initialize(IPluginHost host)
+        {
+            
+            m_host = host;
+            m_gen = new Generator();
+            //ToolStripItemCollection tsMenu = m_host.MainWindow.ToolsMenu.DropDownItems;
+
+            //// Add a separator at the bottom
+            //m_tsSeparator = new ToolStripSeparator();
+            //tsMenu.Add(m_tsSeparator);
+
+            //// Add menu item 'Do Something'
+            //m_tsmiMenuItem = new ToolStripMenuItem();
+            //m_tsmiMenuItem.Text = "Do Something";
+            //m_tsmiMenuItem.Click += this.OnMenuDoSomething;
+            //tsMenu.Add(m_tsmiMenuItem);
+            m_host.PwGeneratorPool.Add(m_gen);
+            return true;
+            //return base.Initialize(host);
+        }
+
+        public override void Terminate()
+        {
+            ToolStripItemCollection tsMenu = m_host.MainWindow.ToolsMenu.DropDownItems;
+            m_tsmiMenuItem.Click -= this.OnMenuDoSomething;
+            tsMenu.Remove(m_tsmiMenuItem);
+            tsMenu.Remove(m_tsSeparator);
+            //base.Terminate();
+        }
+
+        private void OnMenuDoSomething(object sender, EventArgs e)
+        {
+            // Called when the menu item is clicked
+        }
+    }
+}
